9a47e5a68fe89f4db3a067fc2ffd7cf8ec64a941
Prince Patel <princu09@192.168.1.2>
2023-06-05 00:09:06 +0530

    All Table Screen Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,35 @@ $(document).ready(function () {
     $(".sidebar").toggleClass("active");
   });
 
-  if ($(window).width() < 768) {
-    $(".sidebar").removeClass("active");
-  }
+  $(window).resize(function () {
+    if ($(window).width() > 768) {
+      $(".sidebar").removeClass("active");
+    }
+  });
+});
+
+// Table
+$(document).ready(function () {
+  // Initialize DataTables with language customization
+  $("#myTable").DataTable({
+    language: {
+      info: "",
+      dom: "rt",
+      searching: false,
+      paging: true,
+      pageLength: 5,
+      paginate: {
+        // first: '<i class="fas fa-angle-double-left"></i>',
+        previous: '<i class="fas fa-angle-left"></i>',
+        next: '<i class="fas fa-angle-right"></i>',
+        // last: '<i class="fas fa-angle-double-right"></i>',
+      },
+    },
+  });
+
+  // Remove Show X entries
+  $(".dataTables_length").remove();
+
+  // Remove search functionality
+  $(".dataTables_filter").remove();
 });
